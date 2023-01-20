f032e5248e500c8956e0c70e202fb83bfc2f8b41
Manoj Dhiman <31210682+yhtwyl@users.noreply.github.com>
2023-01-21 02:21:48 +0530

    Add files via upload

--- abbreviationISO4.m
+++ abbreviationISO4.m
@@ -1,42 +1,42 @@
-% function abbreviationISO4(string,ltwa20210702)
-% disp(size(ltwa20210702));
-string = "Journal of Liquid Chromatography and Related Technologies";
+% function abbreviationISO4(string,LTWAISO4Abbreviations)
+% disp(size(LTWAISO4Abbreviations));
+string = "Journal of Liquid Chromatography and Related Technologies"; % SAMPLE JOURNAL NAME
 splitted = split(string);
 % length(splitted(1));
 for i = 1:length(splitted)
     count = 0;
     while true
         query_str = splitted(i).extractBefore(strlength(splitted(i)) - count + 1);
-        idx = strncmpi(ltwa20210702{:,"WORDS"},query_str,strlength(query_str));
+        idx = strncmpi(LTWAISO4Abbreviations{:,"Column1"},query_str,strlength(query_str));
         if strlength(query_str) <= 4
-            fprintf("%s.\n",query_str);
+            fprintf("%s. ",query_str);
             break;
         elseif nnz(idx)
             if nnz(idx) > 1
                 indices = find(idx);
-                [~,v] = sort(ltwa20210702{indices,"WORDS"}.strlength);
-                if (ltwa20210702{indices(v(1)),"WORDS"}.strlength == strlength(query_str))
-                    fprintf("%s\n",ltwa20210702{indices(v(1)),"ABBREVIATIONS"});
+                [~,v] = sort(LTWAISO4Abbreviations{indices,"Column1"}.strlength);
+                if (LTWAISO4Abbreviations{indices(v(1)),"Column1"}.strlength == strlength(query_str))
+                    fprintf("%s ",LTWAISO4Abbreviations{indices(v(1)),"Column2"});
                     break;
-                elseif ((nnz(idx) == 1) && (ltwa20210702{idx,"WORDS"}.strlength ~= strlength(query_str)) && ltwa20210702{idx,"WORDS"}.endsWith('-'))
-                    fprintf("%s\n",ltwa20210702{idx,"ABBREVIATIONS"});
+                elseif ((nnz(idx) == 1) && (LTWAISO4Abbreviations{idx,"Column1"}.strlength ~= strlength(query_str)) && LTWAISO4Abbreviations{idx,"Column1"}.endsWith('-'))
+                    fprintf("%s ",LTWAISO4Abbreviations{idx,"Column2"});
                     break;
                 else
                     count = count + 1;
                 end
-%                fprintf("%s\n",ltwa20210702{indices(v(1)),"ABBREVIATIONS"});
+%                fprintf("%s\n",LTWAISO4Abbreviations{indices(v(1)),"ABBREVIATIONS"});
 %                 break;
-            elseif (nnz(idx) == 1) && (ltwa20210702{idx,"WORDS"}.strlength == strlength(query_str))
-                fprintf("%s\n",ltwa20210702{idx,"ABBREVIATIONS"});
+            elseif (nnz(idx) == 1) && (LTWAISO4Abbreviations{idx,"Column1"}.strlength == strlength(query_str))
+                fprintf("%s ",LTWAISO4Abbreviations{idx,"Column2"});
                 break;
-            elseif ((nnz(idx) == 1) && (ltwa20210702{idx,"WORDS"}.strlength ~= strlength(query_str)) && ltwa20210702{idx,"WORDS"}.endsWith('-'))
-                fprintf("%s\n",ltwa20210702{idx,"ABBREVIATIONS"});
+            elseif ((nnz(idx) == 1) && (LTWAISO4Abbreviations{idx,"Column1"}.strlength ~= strlength(query_str)) && LTWAISO4Abbreviations{idx,"Column1"}.endsWith('-'))
+                fprintf("%s ",LTWAISO4Abbreviations{idx,"Column2"});
                 break;
             end
         else
             count = count + 1;
         end
     end
-    
 end
+fprintf("\n");
 % end
